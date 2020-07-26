eadf3606895cf5ec1e38ba790204a9154d69e82a
Tygore <12038641+easylite376@users.noreply.github.com>
2020-07-26 22:02:20 +0200

    end of video #5

---
+++ android/app/src/main/kotlin/net/mstaehr/firstapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.mstaehr.firstapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child:  Text("hello world!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    ),
+
+  ));
+}
