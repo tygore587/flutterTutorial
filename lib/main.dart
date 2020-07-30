143a790876478b8183f8791e020e464d0c5a1a6d
Tygore <12038641+easylite376@users.noreply.github.com>
2020-07-30 23:25:36 +0200

    container widget to have a container on a page

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,11 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-          child: IconButton(
-            onPressed: () {
-              print('you clicked me!');
-            },
-            icon: Icon(Icons.alternate_email),
-            color: Colors.amber,
-          )
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
+        margin: EdgeInsets.all(30),
+        color: Colors.grey[400],
+        child: Text("hello"),
 
       ),
       floatingActionButton: FloatingActionButton(
