527fc46ab7c592b75c32e68e548037485347f6e8
Tygore <12038641+easylite376@users.noreply.github.com>
2020-08-06 22:45:29 +0200

    column, end of video 12

--- lib/main.dart
+++ lib/main.dart
@@ -6,33 +6,44 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('hello world'),
-          FlatButton(
-            onPressed: () {},
-            color: Colors.amber,
-            child: Text('click me'),
-          ),
-          Container (
-            color: Colors.cyan,
-            padding: EdgeInsets.all(30),
-            child: Text('inside container'),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.red[600],
-      ),
+        appBar: AppBar(
+          title: Text('My first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text('hello'),
+                Text('world'),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.cyan,
+              child: Text('one'),
+            ),
+            Container(
+              padding: EdgeInsets.all(30),
+              color: Colors.pinkAccent,
+              child: Text('two'),
+            ),
+            Container(
+              padding: EdgeInsets.all(40),
+              color: Colors.amber,
+              child: Text('three'),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: ()
+    {},
+    child: Text('click'),
+    backgroundColor: Colors.red[600],
+    ),
     );
   }
 }
